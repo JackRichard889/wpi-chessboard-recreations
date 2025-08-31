f653aee601069d79715a65987c8e4355b07c5db7
Jack Richard <jack@jackrichard.me>
2025-08-31 14:25:20 -0400

    Update to workflow for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isDevelopment = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
@@ -10,7 +12,10 @@ const config = {
 		adapter: adapter({
 			fallback: 'index.html',
 			pages: 'build/'
-		})
+		}),
+		paths: {
+			base: isDevelopment ? '' : process.env.BASE_PATH,
+		}
 	}
 };
 
