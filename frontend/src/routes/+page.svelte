44b53a738150c1f2bc891906d37ee3aef54acf73
PKAravind <129531022+PKAravind@users.noreply.github.com>
2025-10-03 14:55:12 -0400

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -74,9 +74,9 @@ uler<script lang="ts">
 <div class="grid grid-cols-4 bg-white border border-gray-200 rounded-lg shadow-md">
     <div class="p-6">
         <h2 class="font-bold mb-3 text-xl">Euler's Knight Tour</h2>
-        <p class="mb-1">The Swiss mathematician Leonhard Euler worked out this way of making a knight hop from square to square so that it visits every square just once before returning to its starting point.</p>
-        <p class="mb-1">The description continues on this line too.</p>
-        <p class="mb-1">We want this to be a large space that is descriptive and clear.</p>
+        <p class="mb-1">The Swiss mathematician Leonhard Euler worked out this way of making a knight jump from square to square on a chessboard so that it visits every square just once before returning to its starting point.</p>
+        <p class="mb-1">This tour has another remarkable property. If the squares are numbered in the order that the knight visits them, the sum of the numbers in every row and every column of the square is 260.</p>
+        <p class="mb-1">     </p>
     </div>
     <div class="bg-gray-100 col-span-2 flex justify-center p-4">
         <Stage width={ tileSize * boardSize } height={ tileSize * boardSize }>
