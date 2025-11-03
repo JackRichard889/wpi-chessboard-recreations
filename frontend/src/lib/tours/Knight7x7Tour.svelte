e90f7bbca793b7e4e72736a52b20b42f589cbd59
PKAravind <129531022+PKAravind@users.noreply.github.com>
2025-11-03 14:40:28 -0500

    Update Knight7x7Tour.svelte

--- frontend/src/lib/tours/Knight7x7Tour.svelte
+++ frontend/src/lib/tours/Knight7x7Tour.svelte
@@ -1,3 +1,4 @@
 <h2 class="font-bold mb-3 text-xl">Knight Tour (7x7)</h2>
 <p class="mb-1"></p>
 <p class="mb-1"></p>
+<p class="mb-1"> This knight's tour is also a magic square. The sum of the numbers in any row or column is 175. </p>
