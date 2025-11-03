db850d9a53ceaeb307bfb2dd62636e3d668cade7
Jack Richard <jack@jackrichard.me>
2025-11-03 14:29:17 -0500

    Fixes.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div class="p-5">
     <header class="flex flex-wrap items-center mb-4">
-        <img src="/wpi.svg" alt="WPI logo" class="h-8 mr-5" />
+        <img src="wpi.svg" alt="WPI logo" class="h-8 mr-5" />
         <h1 class="font-serif font-semibold text-primary-500 text-xl">Chessboard Recreations</h1>
     </header>
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,8 +2,8 @@
     import {Tours} from "$lib";
     import Chessboard from "$lib/chessboard/Chessboard.svelte";
 
-    let tourIndex = $state(1);
-    let tour = $derived(Tours[tourIndex]);
+    let tourId = $state(1);
+    let tour = $derived(Tours.find(x => x.id === tourId));
 </script>
 
 <main class="grid grid-cols-4 bg-white border border-gray-200 rounded-lg shadow-md">
@@ -11,7 +11,7 @@
         <h2 class="font-bold mb-3 text-white text-xl">Tour Options</h2>
 
         {#each Tours as option (option.id)}
-            <button class="{tourIndex === option.id ? 'underline' : ''} cursor-pointer block mb-2 text-md text-white whitespace-nowrap hover:underline" onclick={() => tourIndex = option.id}>{option.name}</button>
+            <button class="{tourId === option.id ? 'underline' : ''} cursor-pointer block mb-2 text-md text-white whitespace-nowrap hover:underline" onclick={() => tourId = option.id}>{option.name}</button>
         {/each}
     </nav>
 
