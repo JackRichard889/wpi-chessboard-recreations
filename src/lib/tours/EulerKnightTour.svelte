75df5863caa708f1cd8c65c93b8a00d4e890249a
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-03-06 16:14:27 -0500

    Update EulerKnightTour.svelte

--- src/lib/tours/EulerKnightTour.svelte
+++ src/lib/tours/EulerKnightTour.svelte
@@ -2,7 +2,7 @@
 <p class="mb-1">
 	The Swiss mathematician Leonhard Euler worked out this way of making a knight jump from square to
 	square on a chessboard so that it visits every square just once before returning to its starting
-	point.
+	point. The kight can also jump from the last square of the tour back to ist starting point, so this is knowns as a ''reentrant'' tour.
 </p>
 <p class="mb-1">
 	This tour has another remarkable property. If the squares are numbered in the order that the
