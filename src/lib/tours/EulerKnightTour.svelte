bca7d85e433490e9a5439b79479a3a5ed7032af9
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-03-07 11:20:17 -0500

    Update EulerKnightTour.svelte

--- src/lib/tours/EulerKnightTour.svelte
+++ src/lib/tours/EulerKnightTour.svelte
@@ -1,10 +1,16 @@
 <h2 class="mb-3 text-xl font-bold">Euler's Knight Tour</h2>
 <p class="mb-1">
 	The Swiss mathematician Leonhard Euler worked out this way of making a knight jump from square to
-	square on a chessboard so that it visits every square just once before returning to its starting
-	point. The kight can also jump from the last square of the tour back to ist starting point, so this is knowns as a ''reentrant'' tour.
+	square on a chessboard so that it visits every square just once. This kind of tour, in which every one 
+    of a number of connected points is visited just once in the course of visiting them all, is known as a 
+    Hamiltonian path after the Irish mathematician William Rowan Hamilton. Because the knight is able to jump
+    back from the final square to its starting point, this type of tour is known as a "Hamiltonian circuit" or
+    a ''reentrant'' tour.
 </p>
 <p class="mb-1">
 	This tour has another remarkable property. If the squares are numbered in the order that the
-	knight visits them, the sum of the numbers in every row and every column of the square is 260.
+	knight visits them, the sum of the numbers in every row and every column of the square is the same
+    and equal to 260. A set of numbers that can be arranged in the form of a square with this property is
+    often referred to as a "magic square" (although this term is sometimes restricted only to those squares
+    for which the sums along the two diagonals also equal the sums along the rows and columns).
 </p>
