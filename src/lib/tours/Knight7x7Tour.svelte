7828b223ba1431cd63b51c608e8096d632ba8bab
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-03-21 19:29:13 -0400

    Update Knight7x7Tour.svelte

--- src/lib/tours/Knight7x7Tour.svelte
+++ src/lib/tours/Knight7x7Tour.svelte
@@ -1,7 +1,9 @@
 <h2 class="mb-3 text-xl font-bold">Knight Tour (7x7)</h2>
 <p class="mb-1">
-	Like the 5×5 board, the 7×7 board has an odd number of squares (49), so no closed knight's tour
-	exists. This is an open tour that visits all 49 squares exactly once.
+	The 7×7 board has an odd number of squares (49), so no closed (or rentrant) knight's tour
+	is possible because the knight occupies a color opposite to its initial one after an even number of hops. 
+    This is an open tour that visits all 49 squares exactly once. This tour also has a special feature: at times the knight 
+    jumps off the board only to reenter it from the opposite edge to land on the proper square. 
 </p>
 <p class="mb-1">
 	This knight's tour is also a magic tour. If the squares are numbered in the order the knight
