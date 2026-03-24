44cc5eee6440ed08dfc0781a18c6ee9b86be322a
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-03-24 15:02:57 -0400

    Update Knight7x7Tour.svelte

--- src/lib/tours/Knight7x7Tour.svelte
+++ src/lib/tours/Knight7x7Tour.svelte
@@ -4,8 +4,8 @@
 	is possible because the knight jumps to a square of the opposite color at every step and cannot return to to its 
     original square after an even number of steps. 
     This is an open tour that visits all 49 squares exactly once. However, to accomplish this, the knight must 
-    sometimes jump off an edge of the board and reenter it at the same point on the opposite edge to 
-    land on the appropriate square. 
+    sometimes jump off an edge of the board only to reenter it at the same point on the opposite edge before 
+    landing on the appropriate square. 
 </p>
 <p class="mb-1">
 	This knight's tour is also a magic tour. If the squares are numbered in the order the knight
