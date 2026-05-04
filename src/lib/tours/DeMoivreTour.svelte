58bb72418effd4df84ee1d6bb23f7d7fb3f6886e
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-05-04 12:39:01 -0400

    Update DeMoivreTour.svelte

--- src/lib/tours/DeMoivreTour.svelte
+++ src/lib/tours/DeMoivreTour.svelte
@@ -2,7 +2,7 @@
 <p class="mb-1">
 	Abraham de Moivre (1667–1754) was a French mathematician who fled to England after the revocation
 	of the Edict of Nantes. Best known for de Moivre's theorem in complex analysis and his
-	foundational work in probability theory, he was also carried out a systematic study of the
+	foundational work in probability theory, he also carried out a systematic study of the
 	knight's tour problem.
 </p>
 <p class="mb-1">
