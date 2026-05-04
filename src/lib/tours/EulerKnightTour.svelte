86434efe590f5a3d9a14e92188da9338e3e3a2a0
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-05-03 22:13:29 -0400

    Update EulerKnightTour.svelte

--- src/lib/tours/EulerKnightTour.svelte
+++ src/lib/tours/EulerKnightTour.svelte
@@ -1,11 +1,11 @@
 <h2 class="mb-3 text-xl font-bold">Euler's Knight Tour</h2>
 <p class="mb-1">
 	The Swiss mathematician Leonhard Euler worked out this way of making a knight jump from square to
-	square on a chessboard so that it visits every square just once. This kind of tour, in which every one 
-    of a number of connected points is visited just once in the course of visiting them all, is known as a 
+	square on a chessboard so that it visits every square exactly once. This kind of tour, in which every one 
+    of a number of connected points is visited just once by jumping along the lines joining them, is known as a 
     Hamiltonian path after the Irish mathematician William Rowan Hamilton. Because the knight is able to jump
     back from the final square to its starting point, this type of tour is known as a "Hamiltonian circuit" or
-    a ''reentrant'' tour.
+    a ''reentrant tour''.
 </p>
 <p class="mb-1">
 	This tour has another remarkable property. If the squares are numbered in the order that the
@@ -13,4 +13,8 @@
     and equal to 260. A set of numbers that form a square with this property is known as a
     "magic square" (although this term is sometimes restricted only to those squares
     for which the sums along the two diagonals also equal the sums along the rows and columns).
+<p class="mb-1">
+    The Hindu mathematician Nilakantha came up with a number of remarkable solutions to the knight's tour problem 
+ a little over half a century before Euler, but the modern study of the problem was initiated and greatly influenced
+by the work of Euler.    
 </p>
