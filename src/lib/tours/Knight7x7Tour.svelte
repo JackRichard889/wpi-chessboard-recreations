76d756fb16dd65d48314432d2e7d35d37ed3d466
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-08-23 13:18:00 -0400

    Update Knight7x7Tour.svelte

--- src/lib/tours/Knight7x7Tour.svelte
+++ src/lib/tours/Knight7x7Tour.svelte
@@ -3,12 +3,13 @@
 	The 7×7 board has an odd number of squares (49), so no closed (or rentrant) knight's tour on it is
     possible because the knight jumps to a square of the opposite color at every step and cannot return to  
     its original square after an odd number of steps. 
-    This is an open tour that visits all 49 squares just once. However, to accomplish it, the knight must 
-    sometimes jump off an edge of the board only to reenter it at the same point on the opposite edge before 
-    landing on the appropriate square. 
+    This is an open tour that visits all 49 squares exactly once. However, to accomplish it, the knight must 
+    sometimes jump off an edge of the board and then back on to it at the appropriate point on the opposite edge 
+    before continuing with its tour (the square it must land back on can be found by shifting it horizontally or
+    vertically by eight square lengths after it has jumped off the board). 
 </p>
 <p class="mb-1">
-	This knight's tour is also a magic tour. If the squares are numbered in the order the knight
-	visits them, the sum of the numbers in any row or column is 175, the magic constant for a 7×7
-	board, since the numbers 1 through 49 sum to 1225, and 1225 ÷ 7 = 175.
+	This knight's tour is also a magic tour. If the squares are numbered in the order they are
+	visited, the sum of the numbers in any row or column of the board is 175, the magic constant for a 7×7
+	square, since the numbers 1 through 49 sum to 1225, which is 175 x 7.
 </p>
