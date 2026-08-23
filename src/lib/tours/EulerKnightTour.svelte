83790b7070047da06ff07b0b81dfd73b57a62f02
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-08-23 12:45:26 -0400

    Update EulerKnightTour.svelte

--- src/lib/tours/EulerKnightTour.svelte
+++ src/lib/tours/EulerKnightTour.svelte
@@ -4,17 +4,16 @@
 	square on a chessboard so that it visits every square exactly once. This kind of tour, in which every one 
     of a number of connected points is visited just once by jumping along the lines joining them, is known as a 
     Hamiltonian path after the Irish mathematician William Rowan Hamilton. Because the knight is able to jump
-    back from the final square to its starting point, this type of tour is known as a "Hamiltonian circuit" or
-    a ''reentrant tour''.
+    back from the final square it visits to its starting point, this type of tour is known as a "Hamiltonian circuit" or
+    ''reentrant tour''.
 </p>
 <p class="mb-1">
-	This tour has another remarkable property. If the squares are numbered in the order that the
-	knight visits them, the sum of the numbers in every row and every column of the square is the same
-    and equal to 260. A set of numbers with this property is known as a
-    "magic square" (although the term is sometimes applied only to squares
-    for which the sums along the two diagonals also equal the sums along the rows and columns).
+	This tour has another remarkable property. If the squares are numbered in the order they are
+	visited, the sum of the numbers in every row and column of the chessboard is the same
+    and equal to 260. A set of numbers with this property is known as a "magic square" (although the 
+    term is sometimes used only for squares for which the sums along the two diagonals also equal the sums 
+    along the rows and the columns).
 <p class="mb-1">
     The Hindu mathematician Nilakantha came up with solutions to the knight's tour problem 
- a little over half a century before Euler, but Euler's work was influential in sparking much
-of the later work on the problem.    
+ a little over half a century before Euler, but Euler's work sparked much of the later work on the problem.    
 </p>
