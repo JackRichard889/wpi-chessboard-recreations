c98e2a5122d89a58bfe3163974aa45c9caf8c476
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-08-23 16:32:04 -0400

    Update Knight7x7Tour.svelte

--- src/lib/tours/Knight7x7Tour.svelte
+++ src/lib/tours/Knight7x7Tour.svelte
@@ -4,9 +4,9 @@
     possible because the knight jumps to a square of the opposite color at every step and cannot return to  
     its original square after an odd number of steps. 
     This is an open tour that visits all 49 squares exactly once. However, to accomplish it, the knight must 
-    sometimes jump off an edge of the board and then back on to it at the appropriate point on the opposite edge 
-    before continuing with its tour (the square it must land back on can be found by shifting it horizontally or
-    vertically by eight square lengths after it has jumped off the board). 
+    sometimes jump off an edge of the board and then make its way back to the appropriate square  
+    before continuing with its tour (the appropriate square is the one the knight would land on if the 
+    opposite edges of the board were glued together, so that the board got transformed into a torus). 
 </p>
 <p class="mb-1">
 	This knight's tour is also a magic tour. If the squares are numbered in the order they are
