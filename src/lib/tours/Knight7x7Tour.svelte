c1c84d6af2a8a9f84f758592987a8173dfcfe753
PKAravind <129531022+PKAravind@users.noreply.github.com>
2026-09-01 16:23:10 -0400

    Update Knight7x7Tour.svelte

--- src/lib/tours/Knight7x7Tour.svelte
+++ src/lib/tours/Knight7x7Tour.svelte
@@ -1,11 +1,11 @@
 <h2 class="mb-3 text-xl font-bold">Knight Tour (7x7)</h2>
 <p class="mb-1">
-	The 7×7 board has an odd number of squares (49), so no closed (or rentrant) knight's tour on it is
-    possible because the knight jumps to a square of the opposite color at every step and cannot return to  
-    its original square after an odd number of steps. 
+	The 7×7 board has an odd number of squares (49), so no closed (or rentrant) knight's tour is
+    possible on it because the knight jumps to a square of the opposite color at every step and cannot return to  
+    its original square in an odd number of steps. 
     This is an open tour that visits all 49 squares exactly once. However, to accomplish it, the knight must 
-    sometimes jump off an edge of the board and land back on an unvisited square before going on with its tour.   
-    In this case, the square it chooses is the one of the empty ones available to it if the opposite edges of 
+    sometimes jump off an edge of the board and back on to an unvisited square before going on with its tour.   
+    In this case, the square it lands back on is the one of the empty ones available to it if the opposite edges of 
     the board glued together, so that it became a torus. 
 </p>
 <p class="mb-1">
